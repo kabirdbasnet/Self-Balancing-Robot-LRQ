c7c6f6e1e5e64fee0b7bb9d7b107eb0f5e47dd2b
kabirdbasnet <97182682+kabirdbasnet@users.noreply.github.com>
2023-10-19 12:29:34 -0400

    Update Pins.h to match the tumblr's pinout

--- src/Pins.h
+++ src/Pins.h
@@ -2,20 +2,41 @@
 #define Pins_h
 
 #define VOL_MEASURE_PIN A2
+//ultra sonic sensor
 #define ECHO_PIN A3
 #define TRIG_PIN 11
 #define RECV_PIN 9
-#define NUMPIXELS 4
-#define RGB_PIN 3
-#define AIN1 7
-#define PWMA_LEFT 5
-#define BIN1 12
-#define PWMB_RIGHT 6
-#define STBY_PIN 8
-#define ENCODER_LEFT_A_PIN 2
-#define ENCODER_RIGHT_A_PIN 4
-#define IR_SEND_PIN 9
-#define LEFT_RECEIVE_PIN A0
-#define RIGHT_RECEIVE_PIN A1
+
+// Motor Driver and motor control
+#define STBY_PIN 4
+
+
+//right wheels
+#define AIN1 3
+#define AIN2 2
+#define PWMA_LEFT 5 
+
+
+//left wheels
+#define BIN2 8 //8
+#define BIN1 7 //7
+#define PWMB_RIGHT 10 //10
+
+
+//Encoder configuration
+#define ENCODER_RIGHT_A_PIN A0
+#define ENCODER_RIGHT_B_PIN A1
+
+
+
+#define ENCODER_LEFT_A_PIN A3
+#define ENCODER_LEFT_B_PIN A2
+
+//IR sensor pins
+// #define IR_SEND_PIN 9
+// #define LEFT_RECEIVE_PIN A0
+// #define RIGHT_RECEIVE_PIN A1
+
+
 #define KEY_MODE 10
 #endif
